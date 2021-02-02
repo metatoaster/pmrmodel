af27ff988c6635385d5769a7bb97e415c7cdf740
Tommy Yu <tommy.yu@auckland.ac.nz>
2021-02-02 20:08:32 +1300

    Workspace sync feature
    - Can now fetch a workspace.
    - Bookkeeping has been omitted for now.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use git2::Repository;
 use sqlx::sqlite::SqlitePool;
 use sqlx::Done;
 use std::env;
 use structopt::StructOpt;
+
+#[derive(Debug)]
+struct WorkspaceRecord {
+    id: i64,
+    url: String,
+    description: Option<String>,
+}
 
 #[derive(StructOpt)]
 struct Args {
@@ -23,6 +31,9 @@ enum Command {
         #[structopt(short = "l", long = "longdesc", default_value = "")]
         long_description: String,
     },
+    Fetch {
+        id: i64,
+    },
 }
 
 #[async_std::main]
@@ -38,8 +49,26 @@ async fn main(args: Args) -> anyhow::Result<()> {
         }
         Some(Command::Update { id, description, long_description }) => {
             println!("Updating workspace with id {}", id);
-            let workspace_id = update_workspace(&pool, id, description, long_description).await?;
-            println!("Updateed workspace with id {}", id);
+            if update_workspace(&pool, id, description, long_description).await? {
+                println!("Updated workspace id {}", id);
+            }
+            else {
+                println!("Invalid workspace id {}", id);
+            }
+        }
+        Some(Command::Fetch { id }) => {
+            println!("Fetching commits for workspace with id {}", id);
+            let workspace = get_workspaces_by_id(&pool, id).await?;
+            println!(
+                "Got workspace {} - {} - {}",
+                workspace.id,
+                &workspace.url,
+                match &workspace.description {
+                    Some(v) => v,
+                    None => "<empty>",
+                },
+            );
+            git_sync_workspace(&workspace);
         }
         None => {
             println!("Printing list of all workspaces");
@@ -78,7 +107,7 @@ WHERE id = ?3
         "#,
         description,
         long_description,
-        id
+        id,
     )
     .execute(pool)
     .await?
@@ -111,5 +140,30 @@ ORDER BY id
         );
     }
 
+    Ok(())
+}
+
+async fn get_workspaces_by_id(pool: &SqlitePool, id: i64) -> anyhow::Result<WorkspaceRecord> {
+    // ignoring superceded_by_id for now?
+    let rec = sqlx::query_as!(WorkspaceRecord,
+        r#"
+SELECT id, url, description
+FROM workspace
+WHERE id = ?1
+        "#,
+        id,
+    )
+    .fetch_one(pool)
+    .await?;
+    // TODO custom match statement for Err/Ok for custom message
+    Ok(rec)
+}
+
+
+fn git_sync_workspace(workspace: &WorkspaceRecord) -> anyhow::Result<()> {
+    let repo = match Repository::clone(&workspace.url, workspace.id.to_string()) {
+        Ok(repo) => println!("Cloned repo"),
+        Err(e) => panic!("failed to clone: {}", e),
+    };
     Ok(())
 }
